d3ad98d13ad903e44e75a2143bba820e174d093c
Andrey Fedyakov <ya.andreyfedyakov@yandex.ru>
2023-01-03 23:19:24 +0300

    XML-based Configuration. Constructor Injection

--- src/main/java/com/corp/spring/ApplicationRunner.java
+++ src/main/java/com/corp/spring/ApplicationRunner.java
@@ -7,6 +7,7 @@ public class ApplicationRunner {
 
     public static void main(String[] args) {
         var context = new ClassPathXmlApplicationContext("application.xml");
-        System.out.println(context.getBean("p1", ConnectionPool.class));
+        var connectionPool = context.getBean("p1", ConnectionPool.class);
+        System.out.println(connectionPool);
     }
 }

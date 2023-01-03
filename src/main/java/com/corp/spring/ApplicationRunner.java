5fb700d3839e3719b00c578afd7ae27e7bc8bca5
Andrey Fedyakov <ya.andreyfedyakov@yandex.ru>
2023-01-04 00:06:21 +0300

    XML-based Configuration. Factory Method Injection

--- src/main/java/com/corp/spring/ApplicationRunner.java
+++ src/main/java/com/corp/spring/ApplicationRunner.java
@@ -1,6 +1,7 @@
 package com.corp.spring;
 
 import com.corp.spring.database.pool.ConnectionPool;
+import com.corp.spring.database.repository.CompanyRepository;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
@@ -8,6 +9,7 @@ public class ApplicationRunner {
     public static void main(String[] args) {
         var context = new ClassPathXmlApplicationContext("application.xml");
         var connectionPool = context.getBean("p1", ConnectionPool.class);
+        var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
         System.out.println(connectionPool);
     }
 }

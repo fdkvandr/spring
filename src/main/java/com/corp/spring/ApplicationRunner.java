8d0010b16ca5a6cc6bf6b432cab07e9829424472
Andrey Fedyakov <ya.andreyfedyakov@yandex.ru>
2023-01-05 21:42:33 +0300

    XML-based Configuration. Lifecycle Callbacks

--- src/main/java/com/corp/spring/ApplicationRunner.java
+++ src/main/java/com/corp/spring/ApplicationRunner.java
@@ -7,9 +7,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        var context = new ClassPathXmlApplicationContext("application.xml");
-        var connectionPool = context.getBean("p1", ConnectionPool.class);
-        var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
-        System.out.println(connectionPool);
+        try (var context = new ClassPathXmlApplicationContext("application.xml")) {
+
+            var connectionPool = context.getBean("p1", ConnectionPool.class);
+            var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+            System.out.println(connectionPool);
+        }
     }
 }

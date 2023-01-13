36999ea71d9fa733fd6cf98c41c8b95e369ab154
Andrey Fedyakov <ya.andreyfedyakov@yandex.ru>
2023-01-13 22:10:34 +0300

    Java-based Configuration. @Import & @ImportResource

--- src/main/java/com/corp/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/corp/spring/config/ApplicationConfiguration.java
@@ -1,13 +1,13 @@
 package com.corp.spring.config;
 
 import com.corp.spring.database.repository.CrudRepository;
-import org.springframework.context.annotation.ComponentScan;
+import com.corp.web.config.WebConfiguration;
+import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+@ImportResource("classpath:application.xml")
+@Import(WebConfiguration.class)
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "com.corp.spring",
---
+++ src/main/java/com/corp/web/config/WebConfiguration.java
@@ -0,0 +1,8 @@
+package com.corp.web.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfiguration {
+
+}

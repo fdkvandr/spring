8fa9cc7d2320ac81b8379ac55a7b11fb4e6f4054
Andrey Fedyakov <ya.andreyfedyakov@yandex.ru>
2023-01-14 23:59:43 +0300

    Java-based Configuration. @Bean

--- src/main/java/com/corp/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/corp/spring/config/ApplicationConfiguration.java
@@ -1,7 +1,11 @@
 package com.corp.spring.config;
 
+import com.corp.spring.database.pool.ConnectionPool;
 import com.corp.spring.database.repository.CrudRepository;
+import com.corp.spring.database.repository.UserRepository;
 import com.corp.web.config.WebConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.stereotype.Component;
@@ -10,13 +14,29 @@ import org.springframework.stereotype.Component;
 @Import(WebConfiguration.class)
 @Configuration
 @PropertySource("classpath:application.properties")
-@ComponentScan(basePackages = "com.corp.spring",
-useDefaultFilters = false,
-includeFilters = {
-        @Filter(type = FilterType.ANNOTATION, value = Component.class),
-        @Filter(type = FilterType.ASSIGNABLE_TYPE, value = CrudRepository.class),
-        @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository"),
-})
+@ComponentScan(basePackages = "com.corp.spring", useDefaultFilters = false, includeFilters = {@Filter(type = FilterType.ANNOTATION, value = Component.class), @Filter(type = FilterType.ASSIGNABLE_TYPE, value = CrudRepository.class), @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository"),})
 public class ApplicationConfiguration {
 
+    @Bean
+    @Scope(BeanDefinition.SCOPE_SINGLETON)
+    public ConnectionPool pool2(@Value("${db.username}") String username) {
+        return new ConnectionPool(username, 20);
+    }
+
+    @Bean
+    public ConnectionPool pool3() {
+        return new ConnectionPool("test-pool", 20);
+    }
+
+    @Bean
+    public UserRepository userRepository2(ConnectionPool pool2) {
+        return new UserRepository(pool2);
+    }
+
+    @Bean
+    public UserRepository userRepository3() {
+        ConnectionPool connectionPool = pool3();
+        ConnectionPool connectionPool2 = pool3();
+        return new UserRepository(pool3());
+    }
 }
--- src/main/java/com/corp/spring/database/repository/UserRepository.java
+++ src/main/java/com/corp/spring/database/repository/UserRepository.java
@@ -1,14 +1,16 @@
 package com.corp.spring.database.repository;
 
 import com.corp.spring.database.pool.ConnectionPool;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public class UserRepository {
 
     private final ConnectionPool connectionPool;
 
-    public UserRepository(ConnectionPool connectionPool) {
+    public UserRepository(@Qualifier("pool2") ConnectionPool connectionPool) {
         this.connectionPool = connectionPool;
     }
 }

df510170bc27b50e267ac091bb3679e10f119811
Andrey Fedyakov <ya.andreyfedyakov@yandex.ru>
2023-01-18 00:30:21 +0300

    Spring Boot. @ConfigurationProperties

--- src/main/java/com/corp/spring/config/JpaConfiguration.java
+++ src/main/java/com/corp/spring/config/JpaConfiguration.java
@@ -1,14 +1,19 @@
 package com.corp.spring.config;
 
 import com.corp.spring.config.condition.JpaCondition;
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
-
-import jakarta.annotation.PostConstruct;
 
 @Conditional(JpaCondition.class)
 @Configuration
 public class JpaConfiguration {
+
+    // @Bean
+    // @ConfigurationProperties(prefix = "db")
+    // public DatabaseProperties databaseProperties() {
+    //     return new DatabaseProperties();
+    // }
 
     @PostConstruct
     void init() {

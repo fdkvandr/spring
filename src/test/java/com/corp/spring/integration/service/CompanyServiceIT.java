7bec07cea289bc2355c6b7d1dda97488d9f0246d
Andrey Fedyakov <ya.andreyfedyakov@yandex.ru>
2023-01-19 22:46:58 +0300

    Test Starter. Integration Testing

--- src/test/java/com/corp/spring/integration/service/CompanyServiceIT.java
+++ src/test/java/com/corp/spring/integration/service/CompanyServiceIT.java
@@ -1,21 +1,23 @@
 package com.corp.spring.integration.service;
 
+import com.corp.spring.config.DatabaseProperties;
 import com.corp.spring.dto.CompanyReadDto;
+import com.corp.spring.integration.annotation.IT;
 import com.corp.spring.service.CompanyService;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+@IT
+@RequiredArgsConstructor
 public class CompanyServiceIT {
 
     private static final Integer COMPANY_ID = 1;
 
-    @Autowired
-    private CompanyService companyService;
+    private final CompanyService companyService;
+    private final DatabaseProperties databaseProperties;
 
     @Test
     void findById() {

d4849e5f6abe0eeb5ed2c94a736339a69f382437
Andrey Fedyakov <ya.andreyfedyakov@yandex.ru>
2023-01-23 22:14:00 +0300

    Data JPA Repositories. Special parameters - Pageable, Sort

--- src/main/java/com/corp/spring/database/repository/UserRepository.java
+++ src/main/java/com/corp/spring/database/repository/UserRepository.java
@@ -2,11 +2,15 @@ package com.corp.spring.database.repository;
 
 import com.corp.spring.database.entity.Role;
 import com.corp.spring.database.entity.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -24,4 +28,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
             "set u.role = :role " +
             "where u.id in (:ids)")
     int updateRole(Role role, Long... ids);
+
+    Optional<User> findFirstByOrderByIdDesc();
+
+    List<User> findFirst3ByBirthDateBefore(LocalDate birtdhDate, Sort sort);
+
+    List<User> findAllBy(Pageable pageable);
+
 }
--- src/test/java/com/corp/spring/integration/database/repository/UserRepositoryIT.java
+++ src/test/java/com/corp/spring/integration/database/repository/UserRepositoryIT.java
@@ -1,14 +1,17 @@
 package com.corp.spring.integration.database.repository;
 
 import com.corp.spring.database.entity.Role;
+import com.corp.spring.database.entity.User;
 import com.corp.spring.database.repository.UserRepository;
 import com.corp.spring.integration.annotation.IT;
 import lombok.RequiredArgsConstructor;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static java.time.LocalDate.now;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @IT
 @RequiredArgsConstructor
@@ -16,10 +19,41 @@ class UserRepositoryIT {
 
     private final UserRepository userRepository;
 
+
+    @Test
+    void checkPageable() {
+        var pageable = PageRequest.of(1, 2, Sort.by("id"));
+        var allUsers = userRepository.findAllBy(pageable);
+        assertThat(allUsers).hasSize(2);
+    }
+
+    @Test
+    void checkSort() {
+        var sortBy = Sort.sort(User.class);
+        var sort = sortBy.by(User::getFirstname).and(sortBy.by(User::getLastname));
+
+        var allUsers = userRepository.findFirst3ByBirthDateBefore(now(), sort);
+        assertThat(allUsers).hasSize(3);
+    }
+
+    @Test
+    void checkFirst3() {
+        var sortByFistAndLastName = Sort.by("firstname").and(Sort.by("lastname"));
+        var allUsers = userRepository.findFirst3ByBirthDateBefore(now(), sortByFistAndLastName);
+        assertThat(allUsers).hasSize(3);
+    }
+
+    @Test
+    void checkFirst() {
+        var firstUser = userRepository.findFirstByOrderByIdDesc();
+        assertTrue(firstUser.isPresent());
+        firstUser.ifPresent(user -> assertEquals(5L, user.getId()));
+    }
+
     @Test
     void checkUpdate() {
         var ivan = userRepository.findById(1L);
-        assertSame(Role.ADMIN, ivan.get().getRole());
+        ivan.ifPresent(user -> assertSame(Role.ADMIN, user.getRole()));
 
         var resultCount = userRepository.updateRole(Role.USER, 1L, 5L);
         assertEquals(2, resultCount);
@@ -27,12 +61,12 @@ class UserRepositoryIT {
         // var companyName = ivan.get().getCompany().getName();
 
         var sameIvan = userRepository.findById(1L);
-        assertSame(Role.USER, sameIvan.get().getRole());
+        sameIvan.ifPresent(user -> assertSame(Role.USER, user.getRole()));
     }
 
     @Test
     void checkQueries() {
         var users = userRepository.findAllBy("a", "ov");
-        Assertions.assertThat(users).hasSize(3);
+        assertThat(users).hasSize(3);
     }
 }

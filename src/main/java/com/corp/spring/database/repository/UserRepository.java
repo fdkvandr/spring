72ecfc761df24fd9af607a78284548cd969f7a2c
Andrey Fedyakov <ya.andreyfedyakov@yandex.ru>
2023-01-23 19:44:05 +0300

    Data JPA Repositories. @Modifying

--- src/main/java/com/corp/spring/database/repository/UserRepository.java
+++ src/main/java/com/corp/spring/database/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.corp.spring.database.repository;
 
+import com.corp.spring.database.entity.Role;
 import com.corp.spring.database.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -9,11 +11,17 @@ import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Query("SELECT u FROM User u " +
-            "WHERE u.firstname LIKE %:firstname% " +
-            "AND u.lastname LIKE %:lastname%")
+    @Query("select u from User u " +
+            "where u.firstname like %:firstname% " +
+            "and u.lastname like %:lastname%")
     List<User> findAllBy(String firstname, String lastname);
 
     @Query(value = "SELECT u.* FROM USERS u WHERE username = :username", nativeQuery = true)
     List<User> findAllBy(String username);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("update User u " +
+            "set u.role = :role " +
+            "where u.id in (:ids)")
+    int updateRole(Role role, Long... ids);
 }
--- src/test/java/com/corp/spring/integration/database/repository/UserRepositoryIT.java
+++ src/test/java/com/corp/spring/integration/database/repository/UserRepositoryIT.java
@@ -1,16 +1,34 @@
 package com.corp.spring.integration.database.repository;
 
+import com.corp.spring.database.entity.Role;
 import com.corp.spring.database.repository.UserRepository;
 import com.corp.spring.integration.annotation.IT;
 import lombok.RequiredArgsConstructor;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
 @IT
 @RequiredArgsConstructor
 class UserRepositoryIT {
 
     private final UserRepository userRepository;
+
+    @Test
+    void checkUpdate() {
+        var ivan = userRepository.findById(1L);
+        assertSame(Role.ADMIN, ivan.get().getRole());
+
+        var resultCount = userRepository.updateRole(Role.USER, 1L, 5L);
+        assertEquals(2, resultCount);
+
+        // var companyName = ivan.get().getCompany().getName();
+
+        var sameIvan = userRepository.findById(1L);
+        assertSame(Role.USER, sameIvan.get().getRole());
+    }
 
     @Test
     void checkQueries() {

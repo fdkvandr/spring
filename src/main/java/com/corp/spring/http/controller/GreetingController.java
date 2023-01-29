3498d5df2ceb878066a341d5dff66991a5677f88
Andrey Fedyakov <ya.andreyfedyakov@yandex.ru>
2023-01-29 21:51:24 +0300

    Web Starter. @RequestMapping

--- src/main/java/com/corp/spring/http/controller/GreetingController.java
+++ src/main/java/com/corp/spring/http/controller/GreetingController.java
@@ -1,16 +1,21 @@
 package com.corp.spring.http.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("api/v1")
 public class GreetingController {
 
+    @GetMapping("/hello")
     public ModelAndView hello(ModelAndView modelAndView) {
         modelAndView.setViewName("greeting/hello");
         return modelAndView;
     }
 
+    @GetMapping("/by")
     public ModelAndView bye(ModelAndView modelAndView) {
         modelAndView.setViewName("greeting/bye");
         return modelAndView;

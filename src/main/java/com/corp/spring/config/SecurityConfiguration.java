b8f40e61576a3b9a82cab36f8e8e1bd1ffa4d7fd
Andrey Fedyakov <ya.andreyfedyakov@yandex.ru>
2023-02-16 20:23:57 +0300

    Security Starter. HTTP Basic Authentication

--- src/main/java/com/corp/spring/config/SecurityConfiguration.java
+++ src/main/java/com/corp/spring/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.corp.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,11 +15,12 @@ public class SecurityConfiguration {
                 .csrf().disable() // Отсключим csrf, потому что его нужно конфигурировать на наших страничках отдельно, поэтому об этом дальше
                 .authorizeHttpRequests().anyRequest().authenticated() // Только пользователи, которые прошли аутентификацию могут делать любые запросы по нашему приложению
                 .and()
-                .formLogin(login -> login // Тут мы настраиваем нашу страничку логина
-                        .loginPage("/login") // Говорим, что она находится на url /login - это как раз наш @GetMapping /login контроллер, который перенаправит нас на нашу VIEW
-                        .defaultSuccessUrl("/users") // Говорим, что делать в случае успешной аутентификации - иди на path /users ибо раньше он шел на просто /
-                        .permitAll()); // И говорим, что все кто идет на страничку /login имеет доступ, даже если он не аутентифицирвован
-                // Конечно не очень хорошо, что мы доступ к страничкам выдавали и сверху в authorizeHttpRequests и в formLogin но когда мы дойдем чуть дальше, до авторизации, то мы посмотрим как убрать permiteAll и оставить его только в одном месте
+                .httpBasic(Customizer.withDefaults());
+                // .formLogin(login -> login // Тут мы настраиваем нашу страничку логина
+                //         .loginPage("/login") // Говорим, что она находится на url /login - это как раз наш @GetMapping /login контроллер, который перенаправит нас на нашу VIEW
+                //         .defaultSuccessUrl("/users") // Говорим, что делать в случае успешной аутентификации - иди на path /users ибо раньше он шел на просто /
+                //         .permitAll()); // И говорим, что все кто идет на страничку /login имеет доступ, даже если он не аутентифицирвован
+                // // Конечно не очень хорошо, что мы доступ к страничкам выдавали и сверху в authorizeHttpRequests и в formLogin но когда мы дойдем чуть дальше, до авторизации, то мы посмотрим как убрать permiteAll и оставить его только в одном месте
         return http.build();
     }
 }

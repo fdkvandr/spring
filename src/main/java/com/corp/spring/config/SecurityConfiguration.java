8d2825563253f8677b8989bb37c29a398a32c6cc
Andrey Fedyakov <ya.andreyfedyakov@yandex.ru>
2023-02-19 15:08:05 +0300

    Security Starter. Authorization Architecture

--- src/main/java/com/corp/spring/config/SecurityConfiguration.java
+++ src/main/java/com/corp/spring/config/SecurityConfiguration.java
@@ -1,11 +1,13 @@
 package com.corp.spring.config;
 
+import com.corp.spring.database.entity.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfiguration {
@@ -13,19 +15,21 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable() // Отсключим csrf, потому что его нужно конфигурировать на наших страничках отдельно, поэтому об этом дальше
-                .authorizeHttpRequests().anyRequest().authenticated() // Только пользователи, которые прошли аутентификацию могут делать любые запросы по нашему приложению
-                .and()
+                .csrf().disable()
+                .authorizeHttpRequests(urlConfig -> urlConfig
+                        .requestMatchers("/login", "/users/registration", "/v3/api-docks/**", "/swagger-ui/**").permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/users/{\\d+}/delete")).hasAuthority(Role.ADMIN.getAuthority())
+                        .requestMatchers("/admin/**").hasAuthority(Role.ADMIN.getAuthority())
+                        .anyRequest().authenticated())
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/login")
                         .deleteCookies("JSESSIONID"))
                 // .httpBasic(Customizer.withDefaults());
-                .formLogin(login -> login // Тут мы настраиваем нашу страничку логина
-                        .loginPage("/login") // Говорим, что она находится на url /login - это как раз наш @GetMapping /login контроллер, который перенаправит нас на нашу VIEW
-                        .defaultSuccessUrl("/users") // Говорим, что делать в случае успешной аутентификации - иди на path /users ибо раньше он шел на просто /
-                        .permitAll()); // И говорим, что все кто идет на страничку /login имеет доступ, даже если он не аутентифицирвован
-                // Конечно не очень хорошо, что мы доступ к страничкам выдавали и сверху в authorizeHttpRequests и в formLogin но когда мы дойдем чуть дальше, до авторизации, то мы посмотрим как убрать permiteAll и оставить его только в одном месте
+                .formLogin(login -> login
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/users")
+                        .permitAll());
         return http.build();
     }
 

cd566dc73b110db97fccf44279cbcde8ea77d150
Andrey Fedyakov <ya.andreyfedyakov@yandex.ru>
2023-03-02 17:23:38 +0300

    AOP. Aspect-Oriented Programming. AOP. @Before Advice

--- src/main/java/com/corp/spring/aop/FirstAspect.java
+++ src/main/java/com/corp/spring/aop/FirstAspect.java
@@ -1,11 +1,14 @@
 package com.corp.spring.aop;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Slf4j
 public class FirstAspect {
 
     @Pointcut("@within(org.springframework.stereotype.Controller)")
@@ -38,5 +41,11 @@ public class FirstAspect {
 
     @Pointcut("execution(public * com.corp.spring.service.*Service.findById(*))")
     public void anyFindByIdServiceMethod() {
+    }
+
+    @Before("anyFindByIdServiceMethod()")
+    //@Before("execution(public * com.corp.spring.service.*Service.findById(*))")
+    public void addLogging() {
+        log.info("invoked findById method");
     }
 }
